719a4173addb42dd5335eb1b3cd9c1d4ece82cf8
Christian Weigel <christian.weigel@idmt.fraunhofer.de>
2024-04-26 09:42:29 +0200

    add live mode - add program parameters

---
+++ birdnet_mini/live_input.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pyaudio
+
+import datetime
+from threading import Thread
+
+
+class LiveMicrophoneInput(Thread):
+
+    def __init__(self, device_index, sample_rate, sample_queue, lat=None, lon=None):
+        super(LiveMicrophoneInput, self).__init__()
+        self._sample_rate = sample_rate
+        self._sample_queue = sample_queue
+        self._device_index = device_index
+        self._lat = lat
+        self._lon = lon
+
+        self._pa = pyaudio.PyAudio()
+
+        candidate_name_part = "C525"
+        candidate_names = {}
+        info = self._pa.get_host_api_info_by_index(0)
+        num_devices = info.get('deviceCount')
+
+        for i in range(0, num_devices):
+            dev_info = self._pa.get_device_info_by_host_api_device_index(0, i)
+            if (dev_info.get('maxInputChannels')) > 0:
+                print("Input Device id ", i, " - ", dev_info.get('name'))
+                candidate_names[i] = dev_info.get('name')
+                if candidate_name_part in dev_info.get('name'):
+                    candidate_device_index = i
+
+        if device_index is None or device_index not in candidate_names.keys():
+            print(f"Invalid device index - Using device with index {candidate_device_index} "
+                  f"({candidate_names[candidate_device_index]})")
+            self._device_index = candidate_device_index
+        else:
+            if device_index != candidate_device_index:
+                print(f"Using device index {device_index} ({candidate_names[device_index]}) instead of expected "
+                      f"{candidate_device_index} ({candidate_names[candidate_device_index]})")
+            self._device_index = device_index
+
+        # use fixed values for chunk size and overlap
+        self._chunk_size = 3.0
+        self._overlap = 0
+        self._min_len = 1.0
+
+        self._interrupted = False
+
+    def interrupt(self):
+        self._interrupted = True
+
+    def run(self):
+
+        stream = self._pa.open(format=pyaudio.paFloat32,
+                               channels=1,
+                               rate=self._sample_rate,
+                               input=True,
+                               frames_per_buffer=int(self._sample_rate * self._chunk_size),
+                               input_device_index=self._device_index)
+
+        while not self._interrupted:
+            data = stream.read(int(self._sample_rate * self._chunk_size))
+            chunk = np.frombuffer(data, 'float32')
+            chunk_ts = datetime.datetime.now()
+            self._sample_queue.put((chunk, chunk_ts, self._lat, self._lon))
+
+        stream.stop_stream()
+        stream.close()
+        self._pa.terminate()
+
+    @staticmethod
+    def list_devices():
+        pa = pyaudio.PyAudio()
+        info = pa.get_host_api_info_by_index(0)
+        num_devices = info.get('deviceCount')
+        for i in range(0, num_devices):
+            dev_info = pa.get_device_info_by_host_api_device_index(0, i)
+            if (dev_info.get('maxInputChannels')) > 0:
+                print("Input Device id ", i, " - ", dev_info.get('name'))
+        pa.terminate()
--- birdnet_mini/main.py
+++ birdnet_mini/main.py
@@ -4,37 +4,37 @@ import time
 from pathlib import Path
 from queue import Queue
 
+from birdnet_mini.live_input import LiveMicrophoneInput
 from birdnet_mini.model import Model
 from birdnet_mini.file_reader import FileReader
 from birdnet_mini.classifier import BirdClassifier
 from birdnet_mini.transmission import SerialSender
 
 SCRIPT_DIR = Path(__file__).resolve().parent
-AUDIO_FOLDER_PATH = Path("k:") / "science-camp" / "Data"
-METADATA_FILE_PATH = Path("k:") / "science-camp" / "SMM11597_Summary.txt"
-MODEL_FILE_PATH = SCRIPT_DIR / "models" / "BirdNET_GLOBAL_6K_V2.4_Model_FP32.tflite"
-LABELS_FILE_PATH = SCRIPT_DIR / "models" / "BirdNET_GLOBAL_6K_V2.4_Labels.txt"
 
 
-def main():
+def main(args):
 
     sample_queue = Queue()
     result_queue = Queue()
 
-    with open(LABELS_FILE_PATH, "r") as f:
+    with open(args.labels_file, "r") as f:
         labels = f.read().splitlines()
 
-    model = Model(MODEL_FILE_PATH, labels)
+    model = Model(args.model_file, labels)
 
-    # Create the file_reader, classifier, and serial_sender objects
-    file_reader = FileReader(AUDIO_FOLDER_PATH, METADATA_FILE_PATH, 48000, sample_queue)
+    # Create the file_reader, classifier, and serial_sender objects1
+    if args.live:
+        input = LiveMicrophoneInput(0, 48000, sample_queue, args.latitude, args.longitude)
+    else:
+        input = FileReader(args.audio_folder, args.metadata_file, 48000, sample_queue)
     classifier = BirdClassifier(model, sample_queue, result_queue)
     serial_sender = SerialSender(result_queue, "COM4")
 
     # Start the threads
     serial_sender.start()
     classifier.start()
-    file_reader.start()
+    input.start()
 
     # enter main loop
     try:
@@ -42,15 +42,47 @@ def main():
             time.sleep(0.2)
     except KeyboardInterrupt:
         print("[main ] Externally interrupted. Stopping threads.")
-        file_reader.interrupt()
+        input.interrupt()
         classifier.interrupt()
         serial_sender.interrupt()
 
-    file_reader.join()
+    input.join()
     classifier.join()
     serial_sender.join()
 
 
 if __name__ == "__main__":
+
+    AUDIO_FOLDER_PATH = Path("k:") / "science-camp" / "Data"
+    METADATA_FILE_PATH = Path("k:") / "science-camp" / "SMM11597_Summary.txt"
+    MODEL_FILE_PATH = SCRIPT_DIR / "models" / "BirdNET_GLOBAL_6K_V2.4_Model_FP32.tflite"
+    LABELS_FILE_PATH = SCRIPT_DIR / "models" / "BirdNET_GLOBAL_6K_V2.4_Labels.txt"
+
     parser = argparse.ArgumentParser(description="BirdNet Mini")
-    sys.exit(main())
+
+    parser.add_argument("--audio-folder", type=str, default=AUDIO_FOLDER_PATH,
+                        help="Path to the audio folder.")
+    parser.add_argument("--metadata_file", type=str, default=METADATA_FILE_PATH,
+                        help="Path to the metadata file.")
+    parser.add_argument("--model-file", type=str, default=MODEL_FILE_PATH,
+                        help="Path to the model file.")
+    parser.add_argument("--labels-file", type=str, default=LABELS_FILE_PATH,
+                        help="Path to the labels file.")
+    parser.add_argument("--live", action="store_true",
+                        help="Use live microphone input instead of file input.")
+    parser.add_argument("--latitude", type=float, default=50.68337,
+                        help="Latitude of the recording location. Only used for live input.")
+    parser.add_argument("--longitude", type=float, default=10.93209,
+                        help="Longitude of the recording location. Only used for live input.")
+    parser.add_argument("--list-devices",  action="store_true",
+                        help="List available audio devices.")
+    parser.add_argument("--device-index", type=int, default=None,
+                        help="Index of the audio device to use. Only used for live input.")
+
+    args = parser.parse_args()
+
+    if args.list_devices:
+        LiveMicrophoneInput.list_devices()
+        sys.exit(0)
+
+    sys.exit(main(args))
